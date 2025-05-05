fdbf8cb4f373847c658df7b4a625492a86c04134
Simon PAUGET <simon.pauget@sii.fr>
2025-05-05 15:09:28 +0200

    switched Modal component to a Portal from 'react-native-paper'

--- src/components/DropdownModal.js
+++ src/components/DropdownModal.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import {Modal} from 'react-native';
+import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { Portal } from 'react-native-paper';
 
-const DropdownModal = ({visible, statusBarTranslucent, onRequestClose, children}) => {
-  const defaults = {
-    statusBarTranslucent: statusBarTranslucent || false,
-  };
+const DropdownModal = ({ visible, onRequestClose, children }) => {
+  if (!visible) return null;
+
   return (
-    <Modal
-      onRequestClose={onRequestClose}
-      supportedOrientations={['portrait', 'landscape']}
-      animationType="none"
-      transparent={true}
-      statusBarTranslucent={defaults.statusBarTranslucent}
-      visible={visible}>
-      {children}
-    </Modal>
+    <Portal>
+      <TouchableWithoutFeedback onPress={onRequestClose}>
+        <View style={styles.overlay}>
+          <TouchableWithoutFeedback>
+            <View style={styles.dropdownContainer}>
+              {children}
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+      </TouchableWithoutFeedback>
+    </Portal>
   );
 };
+
+const styles = StyleSheet.create({
+  overlay: {
+    position: 'absolute',
+    top: 0, left: 0, right: 0, bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    zIndex: 9999,
+  },
+  dropdownContainer: {
+    height: '100%',
+    width: '100%',
+  },
+});
 
 export default DropdownModal;
